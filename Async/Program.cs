d74705107e941f04356828f4bcdccff9b92ca7ba
TSANG <wai@tsangfamily.org>
2025-03-25 13:59:36 +0000

    Week 9 demos

---
+++ Async/Program.cs
@@ -0,0 +1,106 @@
+﻿using Async;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Xunit;
+
+//Assert.True(ThreadPool.SetMinThreads(1, 1));
+//Assert.True(ThreadPool.SetMaxThreads(2, 1));
+
+AsyncDemo.RunAsync(args).Wait();
+
+//ThreadDemo.RunThread(args);
+
+
+namespace Async
+{
+    class AsyncDemo()
+    {
+        // async qualifier is required to use await keyword
+        public static async Task RunAsync(string[] args)
+        {
+            Console.WriteLine("Start Async Tasks");
+
+            List<Task> tasks = [];
+            for (int  taskNo = 0;  taskNo < 5;  taskNo++)
+            {
+                await FetchDataAsync(taskNo);
+                // Without await, the tasks will run in parallel
+                //tasks.Add(FetchDataAsync(taskNo));
+            }
+
+            // To wait for all tasks to complete, use Task.WhenAll
+            //await Task.WhenAll(tasks);
+
+            Console.WriteLine($"All tasks completed");
+        }
+
+        // async qualifier is required to use await keyword
+        static async Task FetchDataAsync(int taskNo)
+        {
+            Console.WriteLine($"Thread for task {taskNo} is {Thread.CurrentThread.ManagedThreadId}");
+            var rand = new Random();
+            for (var i = 0; i < 100_000; i++)
+            {
+                if (i % 10_000 == 0)
+                {
+                    Console.WriteLine($"Task {taskNo}: waiting");
+                    // using await causes the method to pause here and
+                    // give another task a chance to run
+                    await Task.Delay(rand.Next(10));
+                }
+                if (i%1_000 == 0)
+                {
+                    Console.WriteLine($"Task {taskNo}: i={i}");
+                }
+            }
+            Console.WriteLine($"Task {taskNo} completed");
+        }
+
+    }
+
+    class ThreadDemo()
+    {
+        public static void RunThread(string[] args)
+        {
+            Console.WriteLine("Start Threads");
+
+            var threads = Enumerable.Range(0, 5)
+                .Select(threadNo => new Thread(() => FetchData(threadNo)))
+                .ToList();
+
+            foreach (var thread in threads)
+            {
+                thread.Start();
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+            Console.WriteLine($"Threads Completed");
+
+        }
+
+        static void FetchData(int threadNo)
+        {
+
+            Console.WriteLine($"Thread for thead {threadNo} is {Thread.CurrentThread.ManagedThreadId}");
+            var rand = new Random();
+            for (var i = 0; i < 100_000; i++)
+            {
+                if (i % 10_000 == 0)
+                {
+                    Console.WriteLine($"Thread {threadNo}: waiting");
+                    Thread.Sleep(rand.Next(10));
+                }
+                if (i % 1_000 == 0)
+                {
+                    Console.WriteLine($"Thread {threadNo}: i={i}");
+                }
+            }
+            Console.WriteLine( $"Thread {threadNo} completed");
+        }
+
+    }
+}
---
+++ StructsAndRecords/Program.cs
@@ -0,0 +1,130 @@
+﻿using System.Security.Cryptography;
+using Xunit;
+
+namespace StructsAndRecords
+{
+    interface IInterface1 { }
+
+    public class MyClass
+    {
+        public string? MyProperty { get; set; }
+
+        public MyClass(string? MyProperty = "MyClass Property")
+        {
+            this.MyProperty = MyProperty;
+        }
+
+        public void MyMethod()
+        {
+            Console.WriteLine($"MyClass {MyProperty}");
+        }
+    }
+
+    public struct MyStruct
+    {
+        public string? MyProperty { get; set; }
+
+        public MyStruct(string? MyProperty = "MyStruct Property")
+        {
+            this.MyProperty = MyProperty;
+        }
+
+        public void MyMethod()
+        {
+            Console.WriteLine($"MyStruct {MyProperty}");
+        }
+    }
+
+
+    // This example is just to show that a record is basically
+    // a class with some restrictions.
+    public record MyRecord
+    {
+        public MyRecord(string? myProperty = "MyRecord Property")
+        {
+            MyProperty = myProperty;
+        }
+
+        public string? MyProperty { get; set; }
+        public void MyMethod()
+        {
+            Console.WriteLine($"MyRecord {MyProperty}");
+        }
+    }
+
+    // This is a record with positional parameters, and is the standard way to define a record.
+    public record MyValueObject(string Prop1, string Prop2, int Prop3, MyClass Prop4)
+    {
+        // Methods are not normally defined in records, but it is possible.
+        public void MyMethod()
+        {
+            Console.WriteLine($"MyValueObject {Prop1} {Prop2} {Prop3}");
+            Prop4.MyMethod();
+        }
+    }
+
+    // illegal: class Class1 : MyStruct { }
+    // illegal: class Class2 : MyRecord { }
+    //
+    // illegal: struct Struct2 : MyStruct { }
+    // illegal: struct Struct2 : MyClass { }
+    // legal to inherit interface struct Struct2 : IInterface1 { }
+    // illegal: struct Struct3 : MyRecord { }
+    //
+    //legal: record Record1: MyRecord { }
+    //legal: record Record2 : IInterface1 { }
+    //illegal: record Record3 : MyClass { }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new MyClass();
+            c.MyMethod();
+            var c2 = new MyClass();
+            Assert.True(c != c2); // Not equal because class is a reference type.
+            ClassFunction(c);
+
+            var s = new MyStruct();
+            s.MyMethod();
+            var s2 = new MyStruct();
+            Assert.True(s.MyProperty == s2.MyProperty); // Can't compare structs directly.
+            StructFunction(s);
+
+            var r = new MyRecord();
+            r.MyMethod();
+            var r2 = new MyRecord();
+            Assert.True(r == r2); // Records are value types, so they can be compared directly.
+            RecordFunction(r);
+
+            var vo = new MyValueObject("Prop1", "Prop2", 3, new MyClass());
+            vo.MyMethod();
+        }
+
+        static void ClassFunction(MyClass c)
+        {
+            // The handle of the object is passed to the function
+            // and stored on the stack until the function returns.
+            // The object itself is on the heap, and is probably
+            // shared with other parts of the program.
+            c.MyMethod();
+        }
+
+        static void StructFunction(MyStruct s)
+        {
+            // The entire struct is passed to the function
+            // and stored on the stack until the function returns.
+            // The struct is not shared with other parts of the program.
+            s.MyMethod();
+        }
+
+        static void RecordFunction(MyRecord r)
+        {
+            // The handle of the object is passed to the function
+            // and stored on the stack until the function returns.
+            // The object itself is on the heap, and is probably
+            // shared with other parts of the program.
+            r.MyMethod();
+        }
+    }
+}

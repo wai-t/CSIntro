57ad77a716e9d45be351629b7502af14f1c67d4c
TSANG <wai@tsangfamily.org>
2025-04-26 14:32:59 +0100

    Adapter and Proxy Pattern

---
+++ DesignPatterns/Adapter.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public class Toaster
+    {
+        public void Toast()
+        {
+            Console.WriteLine("Toasting bread");
+        }
+    }
+
+    public class Cooker
+    {
+        public void Cook()
+        {
+            Console.WriteLine("Cooking food");
+        }
+    }
+
+    public class  WashingMachine
+    {
+        public void Wash()
+        {
+            Console.WriteLine("Washing clothes");
+        }
+    }
+
+    public interface IAppliance
+    {
+        void Operate();
+    }
+
+    public class ToasterAdapter : IAppliance
+    {
+        private readonly Toaster _toaster;
+        public ToasterAdapter(Toaster toaster)
+        {
+            _toaster = toaster;
+        }
+        public void Operate()
+        {
+            _toaster.Toast();
+        }
+    }
+
+    public class CookerAdapter : IAppliance
+    {
+        private readonly Cooker _cooker;
+        public CookerAdapter(Cooker cooker)
+        {
+            _cooker = cooker;
+        }
+        public void Operate()
+        {
+            _cooker.Cook();
+        }
+    }
+
+    public class WashingMachineAdapter : IAppliance
+    {
+        private readonly WashingMachine _washingMachine;
+        public WashingMachineAdapter(WashingMachine washingMachine)
+        {
+            _washingMachine = washingMachine;
+        }
+        public void Operate()
+        {
+            _washingMachine.Wash();
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var toaster = new ToasterAdapter(new Toaster());
+            var cooker = new CookerAdapter(new Cooker());
+            var washingMachine = new WashingMachineAdapter(new WashingMachine());
+            //toaster.Toast();
+            //cooker.Cook();
+            //washingMachine.Wash();
+
+            toaster.Operate();
+            cooker.Operate();
+            washingMachine.Operate();
+        }
+    }
+}
---
+++ DesignPatterns/Proxy.cs
@@ -0,0 +1,123 @@
+﻿using DesignPatterns;
+using Microsoft.VisualStudio.TestPlatform.CrossPlatEngine.Client;
+using NewBackend;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    interface IComplicatedImplementation
+    {
+        string GetData();
+    }
+
+    internal class User(IComplicatedImplementation implementation)
+    {
+
+        public void GetDataFromBackend()
+        {
+            var data = implementation.GetData();
+        }
+    }
+
+
+    internal class Proxy : IComplicatedImplementation
+    {
+        //private readonly string backendUrl;
+        public string GetData()
+        {
+            //
+            // connect to backendUrl
+            // e.g. with a HttpClient
+            // 
+            // var client = new HttpClient();
+            // var response = await client.GetAsync(backendUrl);
+            // var data = await response.Content.ReadAsStringAsync();
+            var data = "aoifjoeifjefjoi";
+            return data;
+        }
+    }
+
+}
+
+namespace NewBackend { // runs on another machine
+
+    internal class Backend
+    {
+        public string GetData() // this method is exposed as a http endpoint
+        {
+            // Simulate a complicated implementation
+            return new ComplicatedImplementation().GetData();
+        }
+    }
+
+
+    internal class ComplicatedImplementation : IComplicatedImplementation
+    {
+        public string GetData()
+        {
+            BigComplicatedComputation();
+            BigComplicatedDBQuery();
+            // Simulate a complicated implementation
+            return "Complicated data";
+        }
+
+        private void BigComplicatedComputation()
+        {
+        }
+
+        private void BigComplicatedDBQuery()
+        {
+        }
+    }
+}
+
+namespace Main
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            // This version goes out to the network
+            var proxy = new Proxy();
+            var user = new User(proxy);
+            user.GetDataFromBackend();
+
+            // this version stays inside the process
+            var inprocess = new ComplicatedImplementation();
+            var user2 = new User(inprocess);
+            user2.GetDataFromBackend();
+        }
+    }
+
+    internal class Original
+    {
+        public virtual string GetData()
+        {
+            return GetDataFromBackend();
+        }
+        private string GetDataFromBackend()
+        {
+            var backend = new Backend();
+            var data = backend.GetData();
+            return data;
+        }
+    }
+
+    // This is a caching proxy of the Original class
+    internal class Cache : Original
+    {
+        private string? cachedData = null;
+        public override string GetData()
+        {
+            if (cachedData == null)
+            {
+                cachedData = base.GetData();
+            }
+            return cachedData;
+        }
+    }
+}

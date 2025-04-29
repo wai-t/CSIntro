cf96e6a554f7901dd187258d974ba9860c864e03
TSANG <wai@tsangfamily.org>
2025-04-29 19:35:16 +0100

    Anonymous Classes and Tuples

---
+++ StructsAndRecords/AnonymousClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructsAndRecords
+{
+    internal class AnonymousClass
+    {
+        // class ThinkOfAName {...}
+        public void Method()
+        {
+            List<int> data = new List<int>() { 1, 2, 3, 4, 5 };
+
+            var result = data.Select(x => new //ThinkOfAName()
+            {
+                Number = x,
+                Square = x * x,
+                Cube = x * x * x
+            });
+
+            foreach (var item in result)
+            {
+                Console.WriteLine($"Number: {item.Number}, Square: {item.Square}, Cube: {item.Cube}");
+            }
+
+
+            var result2 = data.Select(x => new  //ThinkOfAName()
+            {
+                Number = x,
+                Square = x * x,
+                Cube = x * x * x
+            });
+
+            result = result2;
+        }
+    }
+}
---
+++ StructsAndRecords/Tuple.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructsAndRecords
+{
+    internal class TestTuple
+    {
+
+        public ( int N, double, string, TestTuple) Method()
+        {
+            return (1, 1.1, "ijoeij", new TestTuple());
+        }
+
+        public void UseTheTuple()
+        {
+            var tuple = Method();
+            Console.WriteLine($"Item1: {tuple.Item1}");
+            Console.WriteLine($"Item1: {tuple.N}");
+            Console.WriteLine($"Item2: {tuple.Item2}");
+            Console.WriteLine($"Item3: {tuple.Item3}");
+            Console.WriteLine($"Item4: {tuple.Item4}");
+        }
+
+        public void Deconstruct(out int P, out double Q, out string R, out TestTuple S)
+        {
+            P = 1;
+            Q = 1.1;
+            R = "ijoeij";
+            S = new TestTuple();
+        }
+
+        public void UseDeconstruct()
+        {
+            var c = new TestTuple();
+
+            // N.B not a tuple
+            (int P, double Q, string R, TestTuple S) = c;
+            Console.WriteLine($"Item1: {P}");
+            Console.WriteLine($"Item2: {Q}");
+            Console.WriteLine($"Item3: {R}");
+            Console.WriteLine($"Item4: {S}");
+        }
+    }
+}

5c3a5be9052899034eac167f6bbc58ba1ed3ed9d
wai-t <wai@tsangfamily.org>
2025-05-05 16:19:15 +0100

    Maths

---
+++ Math/Program.cs
@@ -0,0 +1,28 @@
+﻿var rng = new Random();
+var sum = 0;
+for (var num = 1; num < 100_000_000; num++)
+{
+    var x = rng.NextDouble();
+    var y = rng.NextDouble();
+    if (x * x + y * y < 1.0)
+        sum++;
+    if ((num % 1000 == 0))
+    {
+        Console.Write($"\r{num} iterations: {4.0 * sum / num}");
+    }
+}
+
+//var rng = new Random();
+//var sum = 0;
+//for (var num = 1; num < 100_000_000; num++)
+//{
+//    var x = rng.NextDouble();
+//    var y = rng.NextDouble();
+//    var z = rng.NextDouble();
+//    if (x * x + y * y + z * z < 1.0)
+//        sum++;
+//    if ((num % 1000 == 0))
+//    {
+//        Console.Write($"\r{num} iterations: {6.0 * sum / num}");
+//    }
+//}
